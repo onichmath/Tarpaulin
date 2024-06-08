9fd37db636acbe944b362f080851dd2ca653e82d
Matthew <mathonich@yahoo.com>
2024-06-08 13:51:10 -0700

    Error codes

---
+++ utils/errorCodes.js
@@ -0,0 +1,22 @@
+module.exports = {
+    400: {
+        code: 400,
+        message: 'The request body was either not present or did not contain the required fields'
+    },
+    401: {
+        code: 401,
+        message: 'The specified credentials were invalid.'
+    },
+    403: {
+        code: 403,
+        message: 'The request was not made by an authenticated User satisfying the authorization criteria described above.'
+    },
+    404: {
+        code: 404,
+        message: 'Specified object not found.'
+    },
+    500: {
+        code: 500,
+        message: 'An internal server error occurred.'
+    }
+}
