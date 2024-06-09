19a4bdfc4d79291850c064829ec52b362c814755
Matthew <mathonich@yahoo.com>
2024-06-08 23:46:26 -0700

    Comments w/ plan for gettingUserByID

--- controllers/Users.js
+++ controllers/Users.js
@@ -34,6 +34,11 @@ module.exports.createUser = function createUser (req, res, next, body) {
 };
 
 module.exports.getUserById = function getUserById (req, res, next, id) {
+  // Instructors and students have differnt response to this endpoint
+  // Checkpermissions AND check if user is the same as the id
+  // WHICH FIRST?
+  // check if user is the same as the id then check permissions then Users.getUserById(id)
+  // ID can be gotten from JWT
   Users.getUserById(id)
     .then(function (response) {
       utils.writeJson(res, response);
